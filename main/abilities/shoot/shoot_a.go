b2bb2e841810888a08a04e19540746da9953db9b
__zalgo__ <jromano416@gmail.com>
2020-01-05 22:23:44 -0800

    add shoot ability selection, animation, and ammo tallying, update ammo ui next

---
+++ main/abilities/shoot/shoot_a.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/tilesources/shoot_a.tilesource\"\n"
+  "default_animation: \"anim\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 12.0
+    y: -4.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- main/abilities/shoot/shoot_ability.go
+++ main/abilities/shoot/shoot_ability.go
@@ -1,10 +1,41 @@
+components {
+  id: "shoot"
+  component: "/main/abilities/shoot/shoot_ability.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"\"\n"
-  "default_animation: \"\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
+  id: "shoot_a"
+  type: "factory"
+  data: "prototype: \"/main/abilities/shoot/shoot_a.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "shoot_b"
+  type: "factory"
+  data: "prototype: \"/main/abilities/shoot/shoot_b.go\"\n"
+  "load_dynamically: false\n"
   ""
   position {
     x: 0.0
