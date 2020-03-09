b0623802faf8d24938f519734e5bec28688a5f64
__zalgo__ <jromano416@gmail.com>
2020-03-09 02:07:01 -0700

    added bats but need to fix their damage next (hitting too fast)

---
+++ main/enemies/bats/bat_spawner.go
@@ -0,0 +1,51 @@
+components {
+  id: "bat_spawner"
+  component: "/main/enemies/bats/bat_spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bat_a"
+  type: "factory"
+  data: "prototype: \"/main/enemies/bats/bat_a.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bat_b"
+  type: "factory"
+  data: "prototype: \"/main/enemies/bats/bat_b.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
